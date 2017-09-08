c69ddad13b24bcc80876bd95d955f124a977da46
Prashant Chamoli <prashantchamoli.165@gmail.com>
2017-09-07 16:02:28 -1200

    Latest commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,39 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+  
+ var formattedName=HTMLheaderName.replace("%data%","Prashant Chamoli");
+ var formattedRole=HTMLheaderRole.replace("%data%","Web Developer");
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ var bio={
+"name" : "Prashant Chamoli",
+"role" : "Web Developer",
+"contacts": {
+"mobile" : "+91-9897482279",
+"facebook" : "prashantchamoli05",
+"linkedin" : "prashantchamoli05"
+"github" : "prashantchamoli05",
+"location" : "New Delhi, India"},
+"welcomeMessage" : "welcome",
+"skills": ["awesomeness","programming","many more to tell"],
+"biopic" : "images/fry.jpg"
+
+
+}
+
+
+
+
+var work={};
+work.position="Web Developer";
+work.employer="XYZ";
+work.years=0.3;
+
+var education={};
+education["name"]="BTech";
+education["years"]=4;
+ education["city"]="New Delhi";
+ $("#main").append(work["position"]);
+$("#main").append(education.name);
+*/
